f4452fc356bf835867c43a728b5ab9fc2d7db74e
BiljanaSi <bilja.test.sajt@gmail.com>
2026-03-02 15:12:28 +0100

    add product sorting tests
    add product sorting validation (A-Z, Z-A, price low/high)

---
+++ pages/ProductsPage.js
@@ -0,0 +1,85 @@
+class ProductsPage {
+    
+    constructor(page){
+        this.page = page;
+        this.sort = page.locator('select[data-test="sort"]');
+        this.productNames = page.locator('h5[data-test="product-name"]');
+        this.selectitem = page.locator('img[alt="Combination Pliers"]');
+        this.price = page.locator('span[data-test="unit-price"]');
+        this.addtocard = page.locator('[data-test="add-to-cart"]');
+        
+    }
+
+    async goto() {
+        await this.page.goto('https://practicesoftwaretesting.com/');
+    }
+    
+     async clickOnSortAsc(){
+        await this.sort.click();
+        await this.sort.selectOption({ value : "name,asc" });  
+    }
+
+      async verifyFirstProductStartsWithA() {
+
+        const firstProductName = (await this.productNames.first().textContent()).trim();
+
+        if (!firstProductName.startsWith('A')) {
+            throw new Error(`First product does not start with A: ${firstProductName}`);
+        }
+
+        console.log(`First product verified: ${firstProductName}`);
+    }
+
+      async getItemsText() {
+        return await this.productNames.allTextContents();
+    }
+
+    async isSortedDesc() {
+        await this.sort.click();
+        await this.sort.selectOption({ value : "name,desc" });  
+
+        const texts = await this.getItemsText();
+        for (let i = 0; i < texts.length - 1; i++) {
+            if (texts[i].localeCompare(texts[i + 1]) < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+     async clickOnSortPriceLow() {
+        await this.sort.click();
+        await this.sort.selectOption({value : "price,desc"});
+    }
+
+    async clickOnSortPriceHigh() {
+        await this.sort.click();
+        await this.sort.selectOption({value : "price,asc"});
+    }
+
+
+    async getPrices() {
+        const priceTexts = await this.price.allTextContents();
+        return priceTexts.map(p => parseFloat(p.replace('$', '')));
+    }
+
+    async isPriceSortedAsc() {
+        const prices = await this.getPrices();
+        for (let i = 0; i < prices.length - 1; i++) {
+            if (prices[i] > prices[i + 1]) return false;
+        }
+        return true;
+    }
+
+    async isPriceSortedDesc() {
+        const prices = await this.getPrices();
+        for (let i = 0; i < prices.length - 1; i++) {
+            if (prices[i] < prices[i + 1]) return false;
+        }
+        return true;
+    }
+  }
+
+
+
+export default {ProductsPage};
---
+++ tests/productpage.spec.js
@@ -0,0 +1,57 @@
+import { test, expect } from '@playwright/test'
+import { user, billingData, paymentData } from '../test-data/userData';
+const { LogIn } = require('../pages/LogIn').default;
+const { CheckOut } = require('../pages/CheckOut').default;
+const { ProductsPage } = require('../pages/ProductsPage').default;
+
+test.describe('Shopping Cart & Checkout Flow', () => {
+  
+  let loginPage;
+  let checkoutPage;
+  let productsPage;
+
+test.beforeEach(async ({ page }) => {
+
+    loginPage = new LogIn(page);
+    checkoutPage = new CheckOut(page);
+    productsPage = new ProductsPage(page);
+
+    await loginPage.goto();
+
+    await expect(page).toHaveURL('https://practicesoftwaretesting.com/');
+
+    await checkoutPage.clickHome();
+  });
+
+ test('verify that products are sorted alphabetically when A-Z filter is applied', async ({ page }) => {
+
+    await  productsPage.clickOnSortAsc();
+
+    await productsPage.verifyFirstProductStartsWithA();
+
+  });
+
+   test('verify that products are sorted alphabetically when Z-A filter is applied', async ({ page }) => {
+
+    await productsPage.isSortedDesc();
+
+    const sorted = await productsPage.isSortedDesc();
+
+    expect(sorted).toBe(true);
+
+ });
+
+  test('products are sorted Low → High when price filter is applied', async () => {
+
+    await productsPage.clickOnSortPriceLow();
+
+    expect(await productsPage.isPriceSortedAsc()).toBe(true);
+  });
+
+  test('products are sorted High → Low when price filter is applied', async () => {
+
+    await productsPage.clickOnSortPriceHigh();
+
+    expect(await productsPage.isPriceSortedDesc()).toBe(true);
+  });
+  });
