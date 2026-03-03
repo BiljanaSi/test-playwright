49b2dd0b59a51d2a60c1c927375461024ccfd167
BiljanaSi <bilja.test.sajt@gmail.com>
2026-03-03 14:51:04 +0100

    add test reporting

--- playwright.config.js
+++ playwright.config.js
@@ -23,15 +23,17 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+  ['list'], 
+  ['html', { outputFolder: 'reports/html', open: 'never' }],
+  ['json', { outputFile: 'reports/results.json' }] 
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
+  screenshot: 'only-on-failure', // screenshot samo za failed testove
+  video: 'retain-on-failure',    // video samo za failed testove
+  trace: 'on-first-retry',       // trace za debug prilikom retry-a
+},
 
   /* Configure projects for major browsers */
   projects: [
