c02e061b5379d735b73331adc77d6f81fe0a7cbc
BiljanaSi <bilja.test.sajt@gmail.com>
2026-03-24 11:40:18 +0100

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
   ['list'], 
-  ['html', { outputFolder: 'reports/html', open: 'never' }],
+  ['html', { outputFolder: 'playwright-report', open: 'never' }],
   ['json', { outputFile: 'reports/results.json' }] 
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
